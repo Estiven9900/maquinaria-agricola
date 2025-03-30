db563a67ea765cb246ddc82749c3ed0e1ce76ac8
Nicolas Estiven Soriano Polania <nicolassoriano1404@gmail.com>
2025-03-29 23:04:56 -0500

    Descomente theme de application definition

--- inventario_agricola/settings.py
+++ inventario_agricola/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    #'theme',
+    'theme',
     'maquinaria_agricola',
     'django_browser_reload',
 ]
